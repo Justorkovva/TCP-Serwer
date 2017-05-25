3a79d3e9285b0ebc79cbc24f86d377f9b65086d8
Justorkovva <j.mackos@wp.pl>
2017-05-25 17:25:55 +0200

    Interfejs

---
+++ TCP Serwer/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace TCP_Serwer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Adres = new System.Windows.Forms.TextBox();
+            this.port = new System.Windows.Forms.NumericUpDown();
+            this.info_o_polaczeniu = new System.Windows.Forms.ListBox();
+            this.Start = new System.Windows.Forms.Button();
+            this.Stop = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.port)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Adres
+            // 
+            this.Adres.Location = new System.Drawing.Point(12, 12);
+            this.Adres.Name = "Adres";
+            this.Adres.Size = new System.Drawing.Size(249, 20);
+            this.Adres.TabIndex = 0;
+            this.Adres.Text = "Adres";
+            // 
+            // port
+            // 
+            this.port.Location = new System.Drawing.Point(284, 13);
+            this.port.Maximum = new decimal(new int[] {
+            65535,
+            0,
+            0,
+            0});
+            this.port.Name = "port";
+            this.port.Size = new System.Drawing.Size(210, 20);
+            this.port.TabIndex = 1;
+            // 
+            // info_o_polaczeniu
+            // 
+            this.info_o_polaczeniu.FormattingEnabled = true;
+            this.info_o_polaczeniu.Location = new System.Drawing.Point(12, 49);
+            this.info_o_polaczeniu.Name = "info_o_polaczeniu";
+            this.info_o_polaczeniu.Size = new System.Drawing.Size(482, 95);
+            this.info_o_polaczeniu.TabIndex = 2;
+            // 
+            // Start
+            // 
+            this.Start.Location = new System.Drawing.Point(12, 187);
+            this.Start.Name = "Start";
+            this.Start.Size = new System.Drawing.Size(249, 23);
+            this.Start.TabIndex = 3;
+            this.Start.Text = "Start";
+            this.Start.UseVisualStyleBackColor = true;
+            // 
+            // Stop
+            // 
+            this.Stop.Location = new System.Drawing.Point(284, 187);
+            this.Stop.Name = "Stop";
+            this.Stop.Size = new System.Drawing.Size(210, 23);
+            this.Stop.TabIndex = 4;
+            this.Stop.Text = "Stop";
+            this.Stop.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(530, 261);
+            this.Controls.Add(this.Stop);
+            this.Controls.Add(this.Start);
+            this.Controls.Add(this.info_o_polaczeniu);
+            this.Controls.Add(this.port);
+            this.Controls.Add(this.Adres);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.port)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox Adres;
+        private System.Windows.Forms.NumericUpDown port;
+        private System.Windows.Forms.ListBox info_o_polaczeniu;
+        private System.Windows.Forms.Button Start;
+        private System.Windows.Forms.Button Stop;
+    }
+}
+
